24c5a395669f08f495cad142ab4042da06629d00
kipngeno <kipngenokevin254@gmail.com>
2024-05-29 14:58:36 +0300

    Initial commit

--- 0x02-react_props/task_0/dashboard/src/App.js
+++ 0x02-react_props/task_0/dashboard/src/App.js
@@ -1,19 +1,15 @@
-import logo from './logo.jpeg';
+import Header from './Header/Header';
+import Footer from './Footer/Footer'
 import './App.css';
 
 function App() {
   return (
 	<div>
-    <div className="App-header">
-		<img src={logo} alt="Holberton Logo"></img>
-		<h1>School dashboard</h1>
-	</div>,
-	<div className="App-body">
-		<p>Login to access the full dashboard</p>
-	</div>,
-	<div className="App-footer">
-		<p>Copyright 2020 - holberton School</p>
-	</div>
+		<Header/>
+		<div className="App-body">
+			<p>Login to access the full dashboard</p>
+		</div>
+		<Footer/>
 	</div>
   );
 }
---
+++ 0x02-react_props/task_0/dashboard/src/Footer/Footer.js
@@ -0,0 +1,10 @@
+import './Footer.css'
+function Footer() {
+	return(
+		<div className="App-footer">
+			<p>Copyright 2020 - holberton School</p>
+		</div>
+	);
+}
+
+export default Footer;
