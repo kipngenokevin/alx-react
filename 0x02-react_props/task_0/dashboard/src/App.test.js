1ba3cf6614899342dd91fe171d43c2fee34eaa51
kipngeno <kipngenokevin254@gmail.com>
2024-06-03 21:03:23 +0300

    initial commit

---
+++ 0x02-react_props/task_0/dashboard/src/App.test.js
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('This test should check if the app renders properly', () => {
+	render(<App />)
+}); 
---
+++ 0x02-react_props/task_0/dashboard/src/Footer/Footer.test.js
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react';
+import Footer from './Footer'
+
+test("This should test the footer section", () => {
+	render(<Footer />);
+});
