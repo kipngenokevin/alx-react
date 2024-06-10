8e7d9b1f1134a72285e83934789664900538bce8
kipngeno <kipngenokevin254@gmail.com>
2024-06-10 16:59:08 +0300

    added enzyme for testing

--- 0x02-react_props/task_2/dashboard/src/setupTests.js
+++ 0x02-react_props/task_2/dashboard/src/setupTests.js
@@ -9,3 +9,5 @@ import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
+
+
--- 0x02-react_props/task_4/dashboard/src/setupTests.js
+++ 0x02-react_props/task_4/dashboard/src/setupTests.js
@@ -5,7 +5,14 @@
 import '@testing-library/jest-dom';
 // setupTests.js
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
 
 configure({ adapter: new Adapter() });
+
+
+// src/setupTests.js
+import util from 'util';
+Object.defineProperty(global, 'TextEncoder', {
+  value: util.TextEncoder,
+});
 
